3232e94a6e00f17e9be91e74253bc0dae270ceb9
Steve Simpson <steventroysimpson@gmail.com>
2016-06-06 14:02:17 -0400

    adding new post types

--- scripts/blog_to_Rmd.R
+++ scripts/blog_to_Rmd.R
@@ -1,5 +1,5 @@
 url2Rmd <- function(url) {
-  xx    <- xml2::read_html(vec_url[4], encoding = "UTF-8")
+  xx    <- xml2::read_html(vec_url[2], encoding = "UTF-8")
   title <- xml2::xml_text(rvest::xml_nodes(xx,"title"))
   generator <- get_generator(xx)
   # xml2::xml_attr(rvest::xml_nodes(xx,"meta"), "generator")
@@ -9,7 +9,7 @@ url2Rmd <- function(url) {
   # asc(xml_find_all(test,"//div/p"))
   # xml2::xml_parent()
 
-  classes <-c("entry-content", "post-content", "post", "post-bodycody", "pf-content")
+  classes <-c("entry-content", "post-content", "post", "post-bodycody", "pf-content", "entry")
   content_post <-names(which.max(sapply(classes,function(z) regexpr(z,asc(xx)))))
   bdy <- xml2::xml_find_first(xx,xpath=paste0(".//div[contains(@class, ",shQuote(content_post),")]"))
   # browser()
@@ -27,15 +27,23 @@ url2Rmd <- function(url) {
   # grepl("td class=\"code", asc(bdy))
   # xml_text(xml_nodes(bdy,xpath=".//pre"))
   # if (grepl("<pre>", asc(bdy))){
-  pre_tags <- rvest::xml_nodes(bdy,xpath=".//pre")
+  pre_tags <- rvest::xml_nodes(bdy,xpath=".//div[contains(@class, 'crayon-pre')]")
   if (length(pre_tags)!=0){
-    if (!any(grepl("<p>",xml2::xml_siblings(pre_tags)))) {
+    if (!any(grepl("<p>",xml2::xml_parent(pre_tags)))) {
       pre_tag_content <- pre_tags
       pre_tag_container <- pre_tags
 
+      pre <- lapply(pre_tag_content, function(z) {paste0(
+        htmltools::div(
+          htmltools::pre(class="r"
+                         , htmltools::code(
+                           sQuote(paste(xml_text(z),collapse="\n")))
+          ))
+        )})
 
+      pre <-  xml2::read_xml(asc(paste(c("<div>",pre, "</div>"), collapse="")))
       pre_tag_container <- walk_for_(pre_tag_container,"up","p")
-      xml2::xml_replace(pre_tag_container,pre_tags)
+      xml2::xml_replace(pre_tag_container,pre)
 
 
     }
@@ -71,7 +79,12 @@ url2Rmd <- function(url) {
 bdy
   # div.my_syntax_box  -> <td class="code">
   #   div.crayon-plain-wrap
-  cmd <-sprintf("echo %s | pandoc -r html  -t markdown", sQuote(asc(bdy)))
+n_bdy <- gsub("</article>", "</div>", gsub("<article", "<div", asc(bdy)))
+gsub("</html>", "<body><div>hi</div></body></html>",(rvest::minimal_html("testing")))
+md <- system(sprintf("echo '%s' | pandoc -r html  -t markdown", gsub("</html>", "<body><div>hi</div></body></html>",rvest::minimal_html("testing")))
+             , intern = TRUE, ignore.stderr = TRUE)
+
+  cmd <-sprintf("echo '%s' | pandoc -r html  -t markdown", asc(test))
   md <- system(cmd , intern = TRUE, ignore.stderr = TRUE)
 
   hotbod <- gsub("```[ ]{0,1}\\{.*?\\}","```\\{r\\}",md[!grepl("<div|</div>|^\\\\",md)])
